f85ebb5bfaf19d48d59541c9d3623c4354360379
dandres <d4ndres@gmail.com>
2018-03-12 23:14:34 -0500

    agregando Blay y eentos js

--- script/events.js
+++ script/events.js
@@ -1,7 +1,22 @@
 var burguerButton = document.getElementById("burguer-button")
 var mainMenuOption = document.getElementById("mainMenuOption")
+var MediaPhone = window.matchMedia('(max-width: 500px)')
 burguerButton.addEventListener('click', toggleMaineManu)
 burguerButton.addEventListener('touchdown', toggleMaineManu)
+MediaPhone.addListener(activadorMediaPhone)
+
+
+function activadorMediaPhone(){
+  if (MediaPhone.matches) {
+    console.log("media")
+  } else {
+    console.log("no media")
+  }
+}
+
+var bLAzy = new Blazy({
+  selector: 'img'
+});
 
 
 function toggleMaineManu(){
